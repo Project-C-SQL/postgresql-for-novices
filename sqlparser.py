5964729febb3f8e313c0f7c6e7c65ce2e9ca8471
Tatu Sirkeoja <tatu.sirkeoja@tuni.fi>
2022-10-11 18:37:29 +0300

    Refactors sqlparser into a class + adds postgres dialect

--- sqlparser.py
+++ sqlparser.py
@@ -1,9 +1,35 @@
 import sys
 import sqlglot
+from sqlglot.dialects.postgres import Postgres
 from pprint import pprint
 
-def parse(sql: str) -> sqlglot.exp.Expression:
-    return sqlglot.parse_one(sql)
+class SqlParser:
+    # Patches the postgres dialect to recognize bpchar
+    Postgres.Tokenizer.KEYWORDS["BPCHAR"] = sqlglot.TokenType.CHAR
+
+    def __init__(self):
+        self.dialect = "postgres"
+
+    def parse(self, sql: str) -> list[sqlglot.exp.Expression]:
+        """
+        Parses all the statements in 'sql'.
+        'sql' should be a string of one or more postgresql statements (delimited by ';').
+        The last ';' in 'sql' is optional.
+        Throws sqlglot.ParseError on invalid sql.
+        """
+
+        return sqlglot.parse(sql, read=self.dialect)
+
+    def parse_one(self, sql: str) -> sqlglot.exp.Expression:
+        """
+        Parses the first statement in 'sql'.
+        Does not validate that 'sql' contains only 1 statement!
+        'sql' should be a postgresql statement.
+        The trailing ';' in 'sql' is optional.
+        Throws sqlglot.ParseError on invalid sql.
+        """
+
+        return sqlglot.parse_one(sql, read=self.dialect)
 
 USAGE = "usage: sql_parser.py"
 
@@ -18,16 +44,9 @@ def main():
          "WHERE order_total_eur = 0 AND order_total_eur = 100;"
 
     statement = IMPOSSIBLE_STATEMENT
-    sql_expression = parse(statement)
+    parser = SqlParser()
+    sql_expression = parser.parse(statement)
     pprint(sql_expression)
-    print(80 * '=')
-
-    def get_where_expression(sql: str) -> str:
-        parsed = sqlglot.parse_one(sql)
-        where_statement = parsed.find(sqlglot.exp.Where)
-        return where_statement
-    where_expression = get_where_expression(statement)
-    pprint(where_expression)
 
 if __name__ == "__main__":
     main()
