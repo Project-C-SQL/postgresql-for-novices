f8e3a5d98e0694602b4343912707af29623a36b3
Tatu Sirkeoja <tatu.sirkeoja@tuni.fi>
2022-10-11 19:30:25 +0300

    Adds tests and removes sqlparser.py::main()

--- sqlparser.py
+++ sqlparser.py
@@ -1,7 +1,6 @@
 import sys
 import sqlglot
 from sqlglot.dialects.postgres import Postgres
-from pprint import pprint
 
 class SqlParser:
     # Patches the postgres dialect to recognize bpchar
@@ -30,23 +29,3 @@ class SqlParser:
         """
 
         return sqlglot.parse_one(sql, read=self.dialect)
-
-USAGE = "usage: sql_parser.py"
-
-def main():
-    if len(sys.argv) != 1:
-        print(USAGE, file=sys.stderr)
-        exit(1)
-
-    TABLE_NAME = "orders"
-    IMPOSSIBLE_STATEMENT = \
-        f"SELECT * FROM {TABLE_NAME} " \
-         "WHERE order_total_eur = 0 AND order_total_eur = 100;"
-
-    statement = IMPOSSIBLE_STATEMENT
-    parser = SqlParser()
-    sql_expression = parser.parse(statement)
-    pprint(sql_expression)
-
-if __name__ == "__main__":
-    main()
